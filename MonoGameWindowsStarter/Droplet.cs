cbcd92d5e4a36462c6312cf74140f8d460182899
Matthew Link <mlink@win.cis.ksu.edu>
2020-02-04 19:31:35 -0600

    Add droplet class

---
+++ MonoGameWindowsStarter/Droplet.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameWindowsStarter
+{
+    /*
+     * We shall spawn in many droplets of varying radii and colors to fall from the sky
+     * Avoid red ones, catch green ones?
+     * */
+    class Droplet
+    {
+
+        Game1 game;
+        Texture2D texture;
+
+        public float X;
+        public float Y;
+
+        public float Radius;
+
+        // TODO: BoundingSphere???
+
+        public Droplet(float x, float y, float r)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Radius = r;
+        }
+
+        public void LoadContent()
+        {
+            // pass in content manager
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Begin();
+
+            // pull in droplet sphere content/image
+
+            sb.End();
+        }
+
+    }
+}
